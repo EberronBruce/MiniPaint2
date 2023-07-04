12acc859bedd6cc310a417d0d6537c3ccd6e2d6b
Bruce <bruce@redravencomputing.com>
2023-07-03 22:42:58 -0500

    Add onTouchEvent

--- app/src/main/java/com/redravencomputing/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/redravencomputing/minipaint/MyCanvasView.kt
@@ -3,8 +3,13 @@ package com.redravencomputing.minipaint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
+
+private const val STROKE_WIDTH = 12f // has to be float
 
 class MyCanvasView(context: Context) : View(context) {
 
@@ -12,6 +17,28 @@ class MyCanvasView(context: Context) : View(context) {
 	private lateinit var extraBitmap: Bitmap
 
 	private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+	private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+
+	// Set up the paint with which to draw.
+	private val paint = Paint().apply {
+		color = drawColor
+		// Smooths out edges of what is drawn without affecting shape.
+		isAntiAlias = true
+		// Dithering affects how colors with higher-precision than the device are down-sampled.
+		isDither = true
+		style = Paint.Style.STROKE // default: FILL
+		strokeJoin = Paint.Join.ROUND // default: MITER
+		strokeCap = Paint.Cap.ROUND // default: BUTT
+		strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+	}
+
+	private var path = Path()
+
+	private var motionTouchEventX  = 0f
+	private var motionTouchEventY  = 0f
+
+	private var currentX = 0f
+	private var currentY = 0f
 
 	override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
 		super.onSizeChanged(w, h, oldw, oldh)
@@ -25,4 +52,29 @@ class MyCanvasView(context: Context) : View(context) {
 		super.onDraw(canvas)
 		canvas?.drawBitmap(extraBitmap, 0f, 0f, null)
 	}
+
+	override fun onTouchEvent(event: MotionEvent?): Boolean {
+		if (event == null) { return false}
+		motionTouchEventX = event.x
+		motionTouchEventY = event.y
+
+		when (event.action) {
+			MotionEvent.ACTION_DOWN -> touchStart()
+			MotionEvent.ACTION_MOVE -> touchMove()
+			MotionEvent.ACTION_UP -> touchUp()
+		}
+		return true
+	}
+
+	private fun touchStart() {
+		path.reset()
+		path.moveTo(motionTouchEventX, motionTouchEventY)
+		currentX = motionTouchEventX
+		currentY = motionTouchEventY
+	}
+
+	private fun touchMove() {}
+
+	private fun touchUp() {}
+
 }
